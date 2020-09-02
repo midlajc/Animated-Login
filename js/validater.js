6fae8ac9e9eec35eaf9e0976a84ee6062b503301
Midlaj C <mail.midlajc@gmail.com>
2020-09-03 00:56:34 +0530

    background color changed to black

---
+++ js/validater.js
@@ -0,0 +1,12 @@
+function validate(){
+
+    var usr=document.getElementById("uname").value
+    var pw=document.getElementById("password").value
+
+    if(usr==""){
+
+        document.getElementById("uname").style.border='#00000';
+
+    }
+
+}
