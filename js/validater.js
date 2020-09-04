b8c84bc523c9f35195f8ee5931b7878e7e1b45a1
Midlaj C <mail.midlajc@gmail.com>
2020-09-04 20:16:12 +0530

    data validation js added

--- js/validater.js
+++ js/validater.js
@@ -1,12 +1,45 @@
 function validate(){
 
-    var usr=document.getElementById("uname").value
+    var usr=document.getElementById("name").value
     var pw=document.getElementById("password").value
+    var usra=document.getElementById("name")
 
-    if(usr==""){
+    if(usr===""&&pw===""){
 
-        document.getElementById("uname").style.border='#00000';
+        function focusname() {
+            document.getElementById("name").style.border="0.2em solid #3498db"
+          }
+        document.getElementById("name").addEventListener("focus", focusname);
 
-    }
+        function focuspass() {
+            document.getElementById("password").style.border="0.2em solid #3498db"
+          }
+        document.getElementById("password").addEventListener("focus", focuspass);
+
+        document.getElementById("name").style.border="0.2em solid  #CC3333"
+        document.getElementById("password").style.border="0.2em solid  #CC3333"
+        return false;
+
+    }else if(usr===""&&pw!=""){
+
+        function focus() {
+            document.getElementById("name").style.border="0.2em solid #3498db"
+          }
+        document.getElementById("name").addEventListener("focus", focus);
+        document.getElementById("name").style.border="0.2em solid  #CC3333"
+        document.getElementById("password").style.border="0.2em solid #3498db"
+        return false;
+
+    }else if(pw===""&&usr!=""){
+
+        function focus() {
+            document.getElementById("password").style.border="0.2em solid #3498db"
+          }
+        document.getElementById("password").addEventListener("focus", focus);  
+        document.getElementById("name").style.border="0.2em solid #3498db"
+        document.getElementById("password").style.border="0.2em solid  #CC3333"
+        return false;
+
+    }else return true;
 
 }
